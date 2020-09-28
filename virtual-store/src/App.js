cbb84bd6d147c82984de7f7009747e7b67341f88
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-09-28 15:25:53 -0700

    added starter files

---
+++ virtual-store/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../src/Components/Header.js'
+import Footer from '../src/Components/Footer.js'
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+  <>
+  <Header />
+  <Footer />
+  </>
+  );
+}
+
+export default App;
